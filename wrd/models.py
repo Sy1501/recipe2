e0815e0c62261b406f57630a8a36b3aa673c0aa5
Simon Rutledge <sy1501@outlook.com>
2024-07-07 10:30:24 +0000

    status and excrept field added and migration made

--- wrd/models.py
+++ wrd/models.py
@@ -2,16 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
 class Recipe(models.Model):
   title = models.CharField(max_length=100)
-  slug = models.SlugField(max_length=200, unique=True)
-  description = models.TextField()
-  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  description = models.SlugField(max_length=250, unique=True)
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipes")
 
   content = models.TextField()
 
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+  status = models.IntegerField(choices=STATUS, default=0)
+  excerpt = models.TextField(blank=True)
 
   def __str__(self):
     return self.title
